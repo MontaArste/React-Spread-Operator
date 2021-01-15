7d2799ca6c19760dc81f1de7b958d469a96203d2
MontaArste <65498413+MontaArste@users.noreply.github.com>
2021-01-15 12:49:24 +0200

    Managing Component Tree

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,25 @@
 import React, { useState } from "react";
-
+import ToDo from "./ToDo";
 function App() {
+  const [item, setItem] = useState("");
+  const [toDoItem, setToDoItem] = useState([]);
 
-const [item, setItem] = useState("");
-const [toDoItem, setToDoItem] = useState([]);
-
-function handleChange(event){
-  setItem(event.target.value)
-}
-function addItem(){
-  setToDoItem(prevValue=>{
-  return [...prevValue, item]
-  })
-  setItem(" ");
-}
+  function handleChange(event) {
+    setItem(event.target.value);
+  }
+  function addItem() {
+    setToDoItem((prevValue) => {
+      return [...prevValue, item];
+    });
+    setItem(" ");
+  }
+  function deleteItem(id) {
+    setToDoItem((toDoItem) => {
+      return toDoItem.filter((item, index) => {
+        return index !== id;
+      });
+    });
+  }
 
   return (
     <div className="container">
@@ -21,14 +27,16 @@ function addItem(){
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input onChange={handleChange} type="text" value={item}/>
+        <input onChange={handleChange} type="text" value={item} />
         <button onClick={addItem}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-        {toDoItem.map(items => <li>{items}</li>)}
+          {toDoItem.map((items, index) => (
+            <ToDo key={index} id={index} text={items} onChecked={deleteItem} />
+          ))}
         </ul>
       </div>
     </div>
@@ -36,4 +44,3 @@ function addItem(){
 }
 
 export default App;
-
---
+++ src/components/ToDo.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function ToDo(props) {
+  
+  return <li onClick={()=>props.onChecked(props.id)}>{props.text}</li>;
+}
+
+export default ToDo;
